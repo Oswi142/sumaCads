bb7631a30116c996adf7842dd75222583cefad48
oswaldoroman <edwin.roman@ucb.edu.bo>
2023-03-27 23:56:50 -0400

    Escribimos los tests y corremos el primer test

---
+++ src/sumarCad.spec.js
@@ -0,0 +1,26 @@
+describe("Sumar", () => {
+    it("Debería retornar el resultado de 0", () => {
+      expect(sumarCadenas("")).toEqual(0);
+    });
+    xit("Debería retornar el resultado de 1", () => {
+        expect(sumarCadenas("1")).toEqual(1);
+      });
+      xit("Debería retornar el resultado de 1,2", () => {
+        expect(sumarCadenas("1,2")).toEqual(3);
+      });
+      xit("Debería retornar el resultado de varios valores", () => {
+        expect(sumarCadenas("1,2,3,4,5")).toEqual(15);
+      });
+      xit("Debería retornar el resultado de 2 números separados por un guión", () => {
+        expect(sumarCadenas("1-2")).toEqual(3);
+      });
+      xit("Debería retornar el resultado de más de 2 números separados por un guión", () => {
+        expect(sumarCadenas("1-2-3-4-5")).toEqual(15);
+      });
+      xit("Debería retornar el resultado de más de 2 números separados por un guión o una coma", () => {
+        expect(sumarCadenas("1,2,3-4-5")).toEqual(15);
+      });
+      xit("Debería retornar el resultado de más de 2 números separados por un guión o una coma o un delimitador personalizado", () => {
+        expect(sumarCadenas("//[;] 6,3-2;1")).toEqual(12);
+      });
+});
