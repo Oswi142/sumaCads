4cd291632d6163a6eb129d203893dd643d90b0a8
oswaldoroman <edwin.roman@ucb.edu.bo>
2023-03-28 00:04:53 -0400

    Mejoramos la función de suma

--- src/sumarCad.js
+++ src/sumarCad.js
@@ -1,7 +1,16 @@
 function sumarCadenas(cad){
-    return 0;
     let suma = 0;
     suma += +cad;
+    if(cadena.length > 1)
+    {
+        let num1 = + cadena[0];
+        let num2 = +cadena[2];
+        suma = num1 + num2;
+    }
+    else
+    {
+        suma += +cadena;
+    }
     return suma;
 }
 export default sumarCadenas;
