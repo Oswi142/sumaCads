35ce588c766826c4ec1c68014cc44bd5d3099cd0
oswaldoroman <edwin.roman@ucb.edu.bo>
2023-03-28 00:14:19 -0400

    probamos el séptimo test

--- src/sumarCad.spec.js
+++ src/sumarCad.spec.js
@@ -19,7 +19,7 @@ describe("Sumar", () => {
     it("Debería retornar el resultado de más de 2 números separados por un guión", () => {
     expect(sumarCadenas("1-2-3-4-5")).toEqual(15);
     });
-    xit("Debería retornar el resultado de más de 2 números separados por un guión o una coma", () => {
+    it("Debería retornar el resultado de más de 2 números separados por un guión o una coma", () => {
     expect(sumarCadenas("1,2,3-4-5")).toEqual(15);
     });
     xit("Debería retornar el resultado de más de 2 números separados por un guión o una coma o un delimitador personalizado", () => {
